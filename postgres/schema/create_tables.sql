61445c805217a99998de913572bc16c48a7aaefd
SHONMAGA2 <muzendaivyne036@gmail>
2025-12-11 15:21:10 +0200

    saving db tables

---
+++ postgres/schema/create_tables.sql
@@ -0,0 +1,29 @@
+--Users table
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password_hash VARCHAR(200) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+--Payment table
+CREATE TABLE payments (
+    payment_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+    amount NUMERIC(10,2) NOT NULL DEFAULT 0,
+    currency VARCHAR(10) NOT NULL DEFAULT 'ZAR',
+    payment_status VARCHAR(20) NOT NULL DEFAULT 'Inactive',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+--Project metadata table
+CREATE TABLE project_metadata (
+    project_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id),
+    title VARCHAR(100) NOT NULL DEFAULT 'My Project',
+    project_description VARCHAR(500),
+    project_status VARCHAR(10) NOT NULL DEFAULT 'Inactive',
+    created_at TIMES--TAMP CURRENT_TIMESTAMP,
+    last_updated TIMESTAMP CURRENT_TIMESTAMP
+);
