56e7f1396e3fef4c234ebe99f6c07214da5fd2f6
SHOMMAGA2 <muzendaivyne036@gmail.com>
2025-12-15 14:40:39 +0200

    removed refrences

--- postgres/schema/create_tables_duckdb.sql
+++ postgres/schema/create_tables_duckdb.sql
@@ -10,7 +10,7 @@ CREATE TABLE users (
 -- Payments table
 CREATE TABLE payments (
     payment_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    user_id BIGINT REFERENCES users(user_id),
+    user_id BIGINT, -- remove REFERENCES
     amount DOUBLE NOT NULL DEFAULT 0,
     currency VARCHAR(10) NOT NULL DEFAULT 'ZAR',
     payment_status VARCHAR(20) NOT NULL DEFAULT 'Inactive',
@@ -20,7 +20,7 @@ CREATE TABLE payments (
 -- Project metadata table
 CREATE TABLE project_metadata (
     project_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    user_id BIGINT REFERENCES users(user_id),
+    user_id BIGINT, -- remove REFERENCES
     title VARCHAR(100) NOT NULL DEFAULT 'My Project',
     project_description VARCHAR(500),
     project_status VARCHAR(10) NOT NULL DEFAULT 'Inactive',
