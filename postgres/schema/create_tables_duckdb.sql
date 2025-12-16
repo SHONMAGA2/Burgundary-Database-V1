be93fea9a685054e1e30a6d40fc7597090137f85
Burgundary <muzendaivyne036@gmail.com>
2025-12-16 18:43:18 +0200

    Update create_tables_duckdb.sql

--- postgres/schema/create_tables_duckdb.sql
+++ postgres/schema/create_tables_duckdb.sql
@@ -10,8 +10,8 @@ CREATE TABLE users (
 -- Payments table
 CREATE TABLE payments (
     payment_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    user_id BIGINT, -- remove REFERENCES
-    amount DOUBLE NOT NULL DEFAULT 0,
+    user_id BIGINT REFERENCES users(user_id),
+    amount DOUBLE PRECISION NOT NULL DEFAULT 0,
     currency VARCHAR(10) NOT NULL DEFAULT 'ZAR',
     payment_status VARCHAR(20) NOT NULL DEFAULT 'Inactive',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -20,7 +20,7 @@ CREATE TABLE payments (
 -- Project metadata table
 CREATE TABLE project_metadata (
     project_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    user_id BIGINT, -- remove REFERENCES
+    user_id BIGINT REFERENCES users(user_id),
     title VARCHAR(100) NOT NULL DEFAULT 'My Project',
     project_description VARCHAR(500),
     project_status VARCHAR(10) NOT NULL DEFAULT 'Inactive',
