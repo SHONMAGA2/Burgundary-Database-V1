1fe482d747edc408e05645647b7107e1dd62e56f
SHONMAGA2 <muzendaivyne036@gmail.com>
2025-12-17 13:43:46 +0200

    saving db files

--- postgres/schema/create_tables.sql
+++ postgres/schema/create_tables.sql
@@ -1,5 +1,5 @@
 --Users table
-CREATE TABLE users (
+CREATE TABLE auth.users (
     user_id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
@@ -7,4 +7,22 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-cccc
+--Project metadata table
+CREATE TABLE core.project_metadata (
+    project_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES auth.users(user_id) ON DELETE CASCADE,
+    title VARCHAR(100) NOT NULL DEFAULT 'My Project',
+    project_description VARCHAR(500),
+    project_status VARCHAR(10) NOT NULL DEFAULT 'Inactive',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE billing.payments (
+    payment_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES auth.users(user_id) ON DELETE CASCADE,
+    amount NUMERIC(10,2) NOT NULL DEFAULT 0,
+    currency VARCHAR(10) NOT NULL DEFAULT 'ZAR',
+    payment_status VARCHAR(20) NOT NULL DEFAULT 'Inactive',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
