6628b738c8c6e25ee94da5d345267ea152626861
SHONMAGA2 <muzendaivyne036@gmail.com>
2025-12-17 11:29:32 +0200

    renewed my create table code

--- postgres/schema/create_tables.sql
+++ postgres/schema/create_tables.sql
@@ -1,29 +1,29 @@
--- Users table
+--Users table
 CREATE TABLE users (
-    user_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    user_name VARCHAR(50) NOT NULL,
+    user_id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     password_hash VARCHAR(200) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Payments table
+--Payment table
 CREATE TABLE payments (
-    payment_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    user_id BIGINT, -- remove REFERENCES
-    amount DOUBLE NOT NULL DEFAULT 0,
+    payment_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+    amount NUMERIC(10,2) NOT NULL DEFAULT 0,
     currency VARCHAR(10) NOT NULL DEFAULT 'ZAR',
     payment_status VARCHAR(20) NOT NULL DEFAULT 'Inactive',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Project metadata table
+--Project metadata table
 CREATE TABLE project_metadata (
-    project_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    user_id BIGINT, -- remove REFERENCES
+    project_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
     title VARCHAR(100) NOT NULL DEFAULT 'My Project',
     project_description VARCHAR(500),
     project_status VARCHAR(10) NOT NULL DEFAULT 'Inactive',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    last_updated TIMESTAMP CURRENT_TIMESTAMP
 );
