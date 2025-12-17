45458b6bceae12e3a87adf966ba0fdc2edc7edb0
SHONMAGA2 <muzendaivyne036@gmail.com>
2025-12-17 13:25:19 +0200

    deleting node module and json fils

--- postgres/schema/create_tables.sql
+++ postgres/schema/create_tables.sql
@@ -7,23 +7,4 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
---Payment table
-CREATE TABLE payments (
-    payment_id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
-    amount NUMERIC(10,2) NOT NULL DEFAULT 0,
-    currency VARCHAR(10) NOT NULL DEFAULT 'ZAR',
-    payment_status VARCHAR(20) NOT NULL DEFAULT 'Inactive',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
---Project metadata table
-CREATE TABLE project_metadata (
-    project_id SERIAL PRIMARY KEY,
-    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
-    title VARCHAR(100) NOT NULL DEFAULT 'My Project',
-    project_description VARCHAR(500),
-    project_status VARCHAR(10) NOT NULL DEFAULT 'Inactive',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    last_updated TIMESTAMP CURRENT_TIMESTAMP
-);
+cccc
